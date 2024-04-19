39257d621253946bf8a31c82def7e13b00acccab
gabrik <gabriele.baldoni@gmail.com>
2024-04-19 13:04:06 +0200

    chore(log-init): restore default init in bridge
    Signed-off-by: gabrik <gabriele.baldoni@gmail.com>

--- zenoh-bridge-ros1/src/main.rs
+++ zenoh-bridge-ros1/src/main.rs
@@ -251,7 +251,7 @@ async fn main() {
     })
     .expect("Error setting Ctrl+C handler");
 
-    zenoh_util::try_init_log_from_env();
+    zenoh_util::init_log_from_env_or("z=info");
     tracing::info!(
         "zenoh-bridge-ros1 {}",
         zenoh_plugin_ros1::Ros1Plugin::PLUGIN_LONG_VERSION
